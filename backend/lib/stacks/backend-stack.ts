6072e220da60c2e0ea04c50c8885f4ceeaf5e29f
Daylen Smith <daylencsmith@gmail.com>
2025-11-10 21:28:12 -0800

    add constructs to feature

--- backend/lib/stacks/backend-stack.ts
+++ backend/lib/stacks/backend-stack.ts
@@ -1,18 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { DemoCognitoConstruct } from "../Cognito/cognito";
+import { CognitoConstruct } from "../Cognito/cognito";
 import { CRUDGatewayConstruct } from "../ApiGateway/gateway";
 import { DynamoTablesConstruct } from "../DynamoDb/tables";
+import { ListingsFeatureConstruct } from "../features/UserListingCRUD";
 
 export class ProductionStack extends cdk.Stack {
-  public readonly cognitoConstruct: DemoCognitoConstruct;
+  public readonly cognitoConstruct: CognitoConstruct;
   public readonly gatewayConstruct: CRUDGatewayConstruct;
   public readonly dynamoTable: DynamoTablesConstruct;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    this.cognitoConstruct = new DemoCognitoConstruct(this, "Cognito", {
+    this.cognitoConstruct = new CognitoConstruct(this, "Cognito", {
       userPoolArn:
         "arn:aws:cognito-idp:us-west-2:552256739229:userpool/us-west-2_E7ZiSeuwQ",
       adminPoolArn:
@@ -26,5 +27,11 @@ export class ProductionStack extends cdk.Stack {
       "DynamoTables",
       "arn:aws:dynamodb:us-west-2:552256739229:table/usertable"
     );
+
+    const listingFeature = new ListingsFeatureConstruct(this, "ListingFeature", {
+      tables: this.dynamoTable,
+      auth: this.cognitoConstruct,
+      api: this.gatewayConstruct,
+    })
   }
 }
