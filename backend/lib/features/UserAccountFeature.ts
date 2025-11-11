af415f7153fcb6591b4a17295f8e1b8013814997
Daylen Smith <daylencsmith@gmail.com>
2025-11-10 18:19:43 -0800

    refactor lambdas in feature

--- backend/lib/features/UserAccountFeature.ts
+++ backend/lib/features/UserAccountFeature.ts
@@ -18,73 +18,46 @@ export class UserSignupFeatureConstruct extends Construct {
   constructor(scope: Construct, id: string, props: UserSignupFeatureProps) {
     super(scope, id);
 
-    //
-    // --- Lambda ---
-    //
-    const signupLambda = new lambda.Function(this, "UserSignupLambda", {
-      runtime: lambda.Runtime.PYTHON_3_11,
-      handler: "signup.lambda_handler",
-      code: lambda.Code.fromAsset("lambdas/userFeature"),
-      environment: {
-        USER_POOL_ID: props.cognito.userPool.userPoolId,
-        CLIENT_ID: props.cognito.userPoolClient.userPoolClientId,
-        DYNAMODB_TABLE: props.tables.userTable.tableName,
-        CLIENT_SECRET_ARN: props.secretManager.secret.secretArn, // pass the secret ARN
-      },
-    });
+    const createLambda = (name: string, handler: string) =>
+      new lambda.Function(this, name, {
+        runtime: lambda.Runtime.PYTHON_3_11,
+        handler,
+        code: lambda.Code.fromAsset("lambdas/userFeature"),
+        environment: {
+          USER_POOL_ID: props.cognito.userPool.userPoolId,
+          CLIENT_ID: props.cognito.userPoolClient.userPoolClientId,
+          DYNAMODB_TABLE: props.tables.userTable.tableName,
+          CLIENT_SECRET_ARN: props.secretManager.secret.secretArn,
+        },
+      });
 
-    const signinLambda = new lambda.Function(this, "UserSigninLambda", {
-      runtime: lambda.Runtime.PYTHON_3_11,
-      handler: "signin.lambda_handler",
-      code: lambda.Code.fromAsset("lambdas/userFeature"),
-      environment: {
-        USER_POOL_ID: props.cognito.userPool.userPoolId,
-        CLIENT_ID: props.cognito.userPoolClient.userPoolClientId,
-        CLIENT_SECRET_ARN: props.secretManager.secret.secretArn,
-      },
-    });
+    // --- Create Lambdas ---
+    const signupLambda = createLambda("UserSignupLambda", "signup.lambda_handler");
+    const signinLambda = createLambda("UserSigninLambda", "signin.lambda_handler");
+    const confirmUserLambda = createLambda("UserConfirmLambda", "confirm_user.lambda_handler");
 
-    const confirmUserLambda = new lambda.Function(this, "UserConfirmLambda", {
-      runtime: lambda.Runtime.PYTHON_3_11,
-      handler: "confirm_user.lambda_handler",
-      code: lambda.Code.fromAsset("lambdas/userFeature"),
-      environment: {
-        USER_POOL_ID: props.cognito.userPool.userPoolId,
-        CLIENT_ID: props.cognito.userPoolClient.userPoolClientId,
-        CLIENT_SECRET_ARN: props.secretManager.secret.secretArn,
-      },
-    });
-
-    //
-    // --- Permissions ---
-    //
+    // --- Grant Permissions ---
     props.tables.userTable.grantReadWriteData(signupLambda);
 
     props.cognito.userPool.grant(signupLambda, "cognito-idp:SignUp");
-    props.secretManager.secret.grantRead(signupLambda);
-
     props.cognito.userPool.grant(signinLambda, "cognito-idp:AdminInitiateAuth");
-    props.secretManager.secret.grantRead(signinLambda);
-
     props.cognito.userPool.grant(confirmUserLambda, "cognito-idp:ConfirmSignUp", "cognito-idp:AdminGetUser");
+
+    props.secretManager.secret.grantRead(signupLambda);
+    props.secretManager.secret.grantRead(signinLambda);
     props.secretManager.secret.grantRead(confirmUserLambda);
 
-    //
+    // --- Helper function to attach API routes ---
+    const addRoute = (path: string, fn: lambda.Function) => {
+      const resource = props.api.authResource.addResource(path);
+      resource.addMethod("POST", new apigw.LambdaIntegration(fn), {
+        authorizationType: apigw.AuthorizationType.NONE,
+      });
+    };
+
     // --- API Routes ---
-    //
-    const signupResource = props.api.authResource.addResource("signup");
-    signupResource.addMethod("POST", new apigw.LambdaIntegration(signupLambda), {
-      authorizationType: apigw.AuthorizationType.NONE,
-    });
-
-    const signinResource = props.api.authResource.addResource("signin");
-    signinResource.addMethod("POST", new apigw.LambdaIntegration(signinLambda), {
-      authorizationType: apigw.AuthorizationType.NONE,
-    });
-
-    const confirmResource = props.api.authResource.addResource("confirm");
-    confirmResource.addMethod("POST", new apigw.LambdaIntegration(confirmUserLambda), {
-      authorizationType: apigw.AuthorizationType.NONE,
-    });
+    addRoute("signup", signupLambda);
+    addRoute("signin", signinLambda);
+    addRoute("confirm", confirmUserLambda);
   }
 }
