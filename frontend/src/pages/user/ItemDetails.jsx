ab3da785d2aeb8ec9eedcf212315c15d7a075625
izcheung <icheung24@my.bcit.ca>
2025-11-12 13:53:09 -0800

    fix: import ReportPopUp component

--- frontend/src/pages/user/ItemDetails.jsx
+++ frontend/src/pages/user/ItemDetails.jsx
@@ -4,8 +4,7 @@ import SellerCard from "@/components/SellerCard";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import Modal from "@mui/material/Modal";
-import PropTypes from "prop-types";
+import ReportPopUp from "@/components/ReportPopUp";
 
 export default function ItemDetails() {
   const location = useLocation();
@@ -61,7 +60,7 @@ export default function ItemDetails() {
         {/* Description */}
         <div className="flex flex-col  justify-between">
           <div>
-            <h3 className="text-2xl"> Details</h3>
+            <h3 className="text-2xl">Details</h3>
 
             <p>{item?.description}</p>
           </div>
@@ -77,47 +76,3 @@ export default function ItemDetails() {
     </div>
   );
 }
-
-function ReportPopUp(props) {
-  const { open, setOpen } = props;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submit");
-    setOpen(false);
-  };
-  return (
-    <div>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <form
-          onSubmit={handleSubmit}
-          className=" flex  items-center flex-col rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] bg-gray-200 shadow-2xl p-4"
-        >
-          <h3 className="flex flex-col items-center mb-2">Report listing?</h3>
-          <textarea
-            rows={4}
-            className="rounded-md w-full p-2 mb-2"
-            placeholder="Please provide an explanation."
-          ></textarea>
-          <button
-            type="submit"
-            className="rounded-lg bg-red-400 text-white px-2 py-1 hover:bg-red-500"
-          >
-            {" "}
-            Report
-          </button>
-        </form>
-      </Modal>
-    </div>
-  );
-}
-
-ReportPopUp.propTypes = {
-  open: PropTypes.bool.isRequired,
-  setOpen: PropTypes.func.isRequired,
-};
