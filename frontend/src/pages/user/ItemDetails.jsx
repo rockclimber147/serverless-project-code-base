dca513893362b6ad872f060238de669c47133814
izcheung <icheung24@my.bcit.ca>
2025-11-12 12:12:38 -0800

    fix: receive item object as param

--- frontend/src/pages/user/ItemDetails.jsx
+++ frontend/src/pages/user/ItemDetails.jsx
@@ -3,31 +3,24 @@ import { useState } from "react";
 import SellerCard from "@/components/SellerCard";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-export default function ItemDetails(props) {
-  //   const { item } = props;
-  // TODO: replace
-  const item = {
-    id: 1,
-    name: "Item1",
-    price: 12.99,
-    location: "123 Main Street, BC",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-    imageUrl: "https://picsum.photos/seed/item1/300/200",
-  };
+export default function ItemDetails() {
+  const location = useLocation();
 
+  const { item } = location.state || {};
   // TODO: replace
   const user = {
     name: "Name",
   };
+
   const [favourite, setFavourite] = useState(false);
 
   return (
     <div className="flex w-full gap-4 h-screen">
       <div className="flex-[3] flex justify-center items-center">
         <img
-          src={item.imageUrl}
+          src={item?.imageUrl}
           className="object-cover h-full object-cover w-full"
         />
       </div>
@@ -37,7 +30,7 @@ export default function ItemDetails(props) {
         {/* Item main info */}
         <div className="mb-2">
           <div className="flex justify-between">
-            <h2 className="text-4xl">{item.name}</h2>
+            <h2 className="text-4xl">{item?.name}</h2>
             <button onClick={() => setFavourite(!favourite)}>
               {favourite ? (
                 <FaHeart className="text-red-500 text-xl transition-colors duration-200" />
@@ -46,8 +39,8 @@ export default function ItemDetails(props) {
               )}
             </button>
           </div>
-          <p className="text-2xl">${item.price}</p>
-          <p className="text-md">{item.location}</p>
+          <p className="text-2xl">${item?.price}</p>
+          <p className="text-md">{item?.location}</p>
         </div>
 
         <div className="flex w-full gap-2 mb-4">
@@ -64,7 +57,7 @@ export default function ItemDetails(props) {
           <div>
             <h3 className="text-2xl"> Details</h3>
 
-            <p>{item.description}</p>
+            <p>{item?.description}</p>
           </div>
 
           <div>
