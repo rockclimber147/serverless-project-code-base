24287964848a55380a8edf23969c322c88daa46f
alice <alice.in.realityland@gmail.com>
2025-11-12 17:02:18 -0800

    Add search listings by name functionality

---
+++ backend/lambdas/searchListings/helpers.py
@@ -0,0 +1,50 @@
+import boto3
+import decimal
+from boto3.dynamodb.types import TypeDeserializer
+
+dynamodb = boto3.client("dynamodb")
+deserializer = TypeDeserializer()
+
+def search_listing(table_name: str, name: str = None):
+    items = _search_table_by_pagination(table_name, name)
+    return _sort_by_descending_time(items)
+
+def _sort_by_descending_time(listings: any):
+    return sorted(listings, key=lambda x: int(x["created_at"]), reverse=True)
+
+def _search_table_by_pagination(table_name: str, name: str = None):
+    items = []
+    last_evaluated_key = None
+
+    # Loop through paginated scan results
+    while True:
+        scan_kwargs = {"TableName": table_name}
+
+        if name:
+            scan_kwargs["FilterExpression"] = "contains(#n, :val)"
+            scan_kwargs["ExpressionAttributeNames"] = {"#n": "item_name"}
+            scan_kwargs["ExpressionAttributeValues"] = {":val": {"S": name}}
+
+        # paginate search
+        if last_evaluated_key:
+            scan_kwargs["ExclusiveStartKey"] = last_evaluated_key
+
+        response = dynamodb.scan(**scan_kwargs)
+
+        for raw_item in response.get("Items", []):
+            item = {k: _deserialize_value(v) for k, v in raw_item.items()}
+            items.append(item)
+
+        last_evaluated_key = response.get("LastEvaluatedKey")
+
+        if not last_evaluated_key:
+            break
+    
+    return items
+
+def _deserialize_value(value):
+    """Deserialize DynamoDB attribute to JSON-safe Python type."""
+    val = deserializer.deserialize(value)
+    if isinstance(val, decimal.Decimal):
+        return float(val) if val % 1 else int(val)
+    return val
---
+++ backend/lib/features/SearchListing.ts
@@ -0,0 +1,34 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+
+import { DynamoTablesConstruct } from "../DynamoDb/tables";
+import { CRUDGatewayConstruct } from "../ApiGateway/gateway";
+
+interface SearchListingsProps {
+  api: CRUDGatewayConstruct;
+  tables: DynamoTablesConstruct;
+}
+
+export class SearchListingsFeatureConstruct extends Construct {
+  constructor(scope: Construct, id: string, props: SearchListingsProps) {
+    super(scope, id);
+
+    const searchLambda = new lambda.Function(this, "SearchListingsLambda", {
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: "search.lambda_handler",
+      code: lambda.Code.fromAsset("lambdas/searchListings"),
+      environment: {
+        LISTINGS_TABLE: props.tables.listingsTable.tableName,
+      },
+    });
+
+    props.tables.listingsTable.grantReadData(searchLambda);
+
+    const search = props.api.publicResource.addResource("search");
+    search.addMethod("GET", new apigateway.LambdaIntegration(searchLambda), {
+      authorizationType: apigateway.AuthorizationType.NONE,
+    });
+  }
+}
