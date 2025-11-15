e6bd2a10e7be89e636cd599f7309cd91d5077fdc
Hsin Pang <hpang19@my.bcit.ca>
2025-11-15 12:12:54 -0800

    undo temporary change

--- backend/lib/S3/listingPhotos.ts
+++ backend/lib/S3/listingPhotos.ts
@@ -1,45 +1,26 @@
-// import * as cdk from "aws-cdk-lib";
-// import { Construct } from "constructs";
-// import * as s3 from "aws-cdk-lib/aws-s3";
-
-// export class ListingPhotosBucketConstruct extends Construct {
-//   public readonly bucket: s3.Bucket;
-
-//   constructor(scope: Construct, id: string) {
-//     super(scope, id);
-
-//     this.bucket = new s3.Bucket(this, "ListingPhotosBucket", {
-//       bucketName: `listing-photos-${cdk.Stack.of(this).account}-${cdk.Stack.of(this).region}`,
-//       removalPolicy: cdk.RemovalPolicy.RETAIN,
-//       publicReadAccess: true,
-//       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
-//       cors: [
-//         {
-//           allowedOrigins: ["*"],
-//           allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT],
-//           allowedHeaders: ["*"],
-//           exposedHeaders: ["ETag"],
-//         },
-//       ],
-//     });
-//   }
-// }
-
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class ListingPhotosBucketConstruct extends Construct {
-  public readonly bucket: s3.IBucket; // Note: use IBucket when importing
+  public readonly bucket: s3.Bucket;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // Adopt existing bucket
-    this.bucket = s3.Bucket.fromBucketName(
-      this,
-      "ListingPhotosBucket",
-      "listing-photos-552256739229-us-west-2" // the existing bucket name
-    );
+    this.bucket = new s3.Bucket(this, "ListingPhotosBucket", {
+      bucketName: `listing-photos-${cdk.Stack.of(this).account}-${cdk.Stack.of(this).region}`,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
+      cors: [
+        {
+          allowedOrigins: ["*"],
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT],
+          allowedHeaders: ["*"],
+          exposedHeaders: ["ETag"],
+        },
+      ],
+    });
   }
 }
