476a88c6db22671dd119a318a2bdc7c4e6a5b4cc
Patricia <pkwlo1992@gmail.com>
2025-11-14 22:00:40 -0800

    add endpoints.ts

---
+++ admin-frontend/src/api/endpoints.ts
@@ -0,0 +1,7 @@
+export const API_BASE_URL =
+  "https://i94mrsytqk.execute-api.us-west-2.amazonaws.com/prod";
+
+export const API_ENDPOINTS = {
+  reportedListings: `${API_BASE_URL}/admin/reported-listings`,
+  deletedListings: `${API_BASE_URL}/admin/deleted-listings`,
+};
