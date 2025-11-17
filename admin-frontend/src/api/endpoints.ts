39e7d1811639044303978cb9498c112910300999
Patricia <pkwlo1992@gmail.com>
2025-11-17 01:58:12 -0800

    update endpoint with delete listing

--- admin-frontend/src/api/endpoints.ts
+++ admin-frontend/src/api/endpoints.ts
@@ -2,8 +2,9 @@ export const API_BASE_URL =
   "https://ardhu7a4ye.execute-api.us-west-2.amazonaws.com/prod";
 
 export const API_ENDPOINTS = {
-  reportedListings: `${API_BASE_URL}/admin/reported-listings`,
+  reportedListings: `${API_BASE_URL}/admin/reported-listings`, //GET all listing with reports
   deletedListings: `${API_BASE_URL}/admin/deleted-listings`, //not implemented yet for MVP
-  viewListing: `${API_BASE_URL}/public/listing?id=`,
-  viewAllListings:`${API_BASE_URL}/public/search`
+  viewListing: `${API_BASE_URL}/public/listing?id=`, //GET listing by ID
+  viewAllListings:`${API_BASE_URL}/public/search`,
+  deleteListing: `${API_BASE_URL}/admin/delete-listing`, //reverses is_removed
 };
