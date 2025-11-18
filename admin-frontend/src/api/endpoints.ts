25de82a46bf609309c7a94f9d758b2eeb48052ce
Ian Chan <ianchan1108@hotmail.com>
2025-11-17 18:17:03 -0800

    add getUsers endpoint

--- admin-frontend/src/api/endpoints.ts
+++ admin-frontend/src/api/endpoints.ts
@@ -5,6 +5,7 @@ export const API_ENDPOINTS = {
   reportedListings: `${API_BASE_URL}/admin/reported-listings`, //GET all listing with reports
   deletedListings: `${API_BASE_URL}/admin/deleted-listings`, //not implemented yet for MVP
   viewListing: `${API_BASE_URL}/public/listing?id=`, //GET listing by ID
-  viewAllListings:`${API_BASE_URL}/public/search`,
+  viewAllListings:`${API_BASE_URL}/public/search`, //GET all listings
   deleteListing: `${API_BASE_URL}/admin/delete-listing`, //reverses is_removed
+  viewUsers: `${API_BASE_URL}/admin/getUsers`, //GET all users
 };
