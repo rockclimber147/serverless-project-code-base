925a7773300fe27d5192425a28c37d52e71641e5
Patricia <pkwlo1992@gmail.com>
2025-11-25 23:26:40 -0800

    update new endpoint

--- admin-frontend/src/api/endpoints.ts
+++ admin-frontend/src/api/endpoints.ts
@@ -2,8 +2,7 @@ export const API_BASE_URL =
   "https://ardhu7a4ye.execute-api.us-west-2.amazonaws.com/prod";
 
 export const API_ENDPOINTS = {
-  reportedListings: `${API_BASE_URL}/admin/reported-listings`, //GET all listing with reports
-  deletedListings: `${API_BASE_URL}/admin/deleted-listings`, //not implemented yet for MVP
+  reportedAndDeletedListings: `${API_BASE_URL}/admin/reported-listings`, //GET all listing with reports
   viewListing: `${API_BASE_URL}/public/listing?id=`, //GET listing by ID
   viewAllListings:`${API_BASE_URL}/public/search`, //GET all listings
   deleteListing: `${API_BASE_URL}/admin/delete-listing`, //reverses is_removed
