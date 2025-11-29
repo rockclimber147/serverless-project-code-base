4c8e65f939654ab7a2afe7a5463622ef5c57c1e2
Ian Chan <116615928+ianhdchan@users.noreply.github.com>
2025-11-29 09:46:09 +0900

    Ian/add api fix listings (#48)
    * update endpoint for listings, update useListing data formatting
    
    * add getAllListings API lambda

--- admin-frontend/src/api/endpoints.ts
+++ admin-frontend/src/api/endpoints.ts
@@ -4,7 +4,7 @@ export const API_BASE_URL =
 export const API_ENDPOINTS = {
   reportedAndDeletedListings: `${API_BASE_URL}/admin/reported-listings`, //GET all listing with reports
   viewListing: `${API_BASE_URL}/public/listing?id=`, //GET listing by ID
-  viewAllListings:`${API_BASE_URL}/public/search`, //GET all listings
+  viewAllListings:`${API_BASE_URL}/admin/getAllListings`, //GET all listings
   deleteListing: `${API_BASE_URL}/admin/delete-listing`, //reverses is_removed
   viewUsers: `${API_BASE_URL}/admin/getUsers`, //GET all users
 };
