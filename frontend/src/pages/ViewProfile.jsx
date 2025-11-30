efc70525c5e63b833f46c413384abb4b7fa97e0b
Hsin Pang <hpang19@my.bcit.ca>
2025-11-30 13:31:04 -0800

    remove sign out button from seller profile

--- frontend/src/pages/ViewProfile.jsx
+++ frontend/src/pages/ViewProfile.jsx
@@ -173,12 +173,14 @@ export default function Profile({ type = "user" }) {
         </div>
 
         {/* RIGHT SIDE: sign out button */}
-        <button
-          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-          onClick={signOut}
-        >
-          Sign out
-        </button>
+        {type === "user" && (
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+            onClick={signOut}
+          >
+            Sign out
+          </button>
+        )}
       </div>
 
       {/* Listing Section */}
