d8cf8140b48de5ce7a7fcde7eb36cff571e3b3e9
izcheung <icheung24@my.bcit.ca>
2025-12-01 12:34:47 -0800

    fix: edit no category display

--- frontend/src/pages/user/ItemDetails.jsx
+++ frontend/src/pages/user/ItemDetails.jsx
@@ -38,6 +38,7 @@ export default function ItemDetails() {
 
   useEffect(() => {
     // If item is not passed via state, fetch it from the backend
+
     if (!item && listingId) {
       const fetchItem = async () => {
         const fetchedItem = await ListingAPIService.getListingById(listingId);
@@ -168,7 +169,10 @@ export default function ItemDetails() {
             </p>
           </div>
 
-          <Chip label={item?.tags} className="m-1"></Chip>
+          <Chip
+            label={item?.tags?.length ? item.tags[0] : "No listed category"}
+            className="m-1"
+          ></Chip>
         </div>
         {/* Seller Card */}
         <div className="detail-card p-6">
